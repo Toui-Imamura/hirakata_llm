a356515a67db9fa6eb95319f15fb5912648d1d5b
HaruyaEnomto <e1j23014@oit.ac.jp>
2025-12-06 21:29:33 +0900

    初期設定

---
+++ config.py
@@ -0,0 +1,20 @@
+"""プロジェクト設定""" 
+import os # ディレクトリ設定 
+DIR_CORPUS =  "corpus"
+DIR_DATA =  "data"
+DIR_MODEL = "model"
+
+# 入力ファイル 
+EXCEL_FILE = os.path.join(DIR_DATA, "FAQlist.xlsx")
+
+# 生成ファイル 
+JSON_DATASET_FILE = os.path.join(DIR_CORPUS, "FAQdataset.json") 
+# モデル出力 
+MODEL_OUTPUT_DIR = os.path.join(DIR_MODEL, "finetuned") 
+TOKENIZER_DIR = os.path.join(DIR_MODEL, "tokenizer") 
+# パラメータ 
+MODEL_NAME = "unsloth/Meta-Llama-3.1-8B-bnb-4bit" 
+OLLAMA_TIMEOUT = 30 
+MAX_SEQ_LENGTH = 2048 
+MAX_STEPS = 500 
+MAX_DATASET_SIZE = 3000
