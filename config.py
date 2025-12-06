a0617aa467c9b4b82ad5dd49c3503df37d4b0982
HaruyaEnomto <e1j23014@oit.ac.jp>
2025-12-06 23:22:29 +0900

    jsonファイルパス変更

--- config.py
+++ config.py
@@ -9,9 +9,11 @@ EXCEL_FILE = os.path.join(DIR_DATA, "FAQlist.xlsx")
 
 # 生成ファイル 
 JSON_DATASET_FILE = os.path.join(DIR_CORPUS, "FAQdataset.json") 
+
 # モデル出力 
 MODEL_OUTPUT_DIR = os.path.join(DIR_MODEL, "finetuned") 
 TOKENIZER_DIR = os.path.join(DIR_MODEL, "tokenizer") 
+
 # パラメータ 
 MODEL_NAME = "unsloth/Meta-Llama-3.1-8B-bnb-4bit" 
 OLLAMA_TIMEOUT = 30 
