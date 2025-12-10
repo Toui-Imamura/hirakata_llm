6271747ad20506f0f7298d81604e080a1f06e502
HaruyaEnomto <e1j23014@oit.ac.jp>
2025-12-10 14:26:36 +0900

    出力フォルダを指定

--- convert_gguf.py
+++ convert_gguf.py
@@ -25,33 +25,3 @@ model.save_pretrained_gguf(
     quantization_method = "q4_k_m",  # 量子化方法（例: f16, q4_0, q4_k_m など）
 )
 print(f"GGUF形式のモデルを保存しました: {DIR_MERGED_MODEL}")
-
-
-"""Fine-TuninngしたモデルをGGUUF形式に変換する"""
-import os
-from unsloth import FastLanguageModel
-from config import *
-
-# モデルの保存先ディレクトリ --- (*1)
-DIR_MERGED_MODEL = os.path.join(MODEL_OUTPUT_DIR, "model_merged")
-
-# モデルを読み込む（LoRAを含む） --- (*2)
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = MODEL_OUTPUT_DIR, # 保存したフォルダ
-    max_seq_length = MAX_SEQ_LENGTH,
-    dtype = None,
-    load_in_4bit = True,
-)
-
-# 推論モードに設定 --- (*3)
-model.eval()
-FastLanguageModel.for_inference(model)
-
-# GGUF形式で保存（LoRAをマージして保存） --- (*4)
-model.save_pretrained_gguf(
-    DIR_MERGED_MODEL,  # 出力フォルダ名
-    tokenizer = tokenizer,
-    quantization_method = "q4_k_m",  # 量子化方法（例: f16, q4_0, q4_k_m など）
-)
-print(f"GGUF形式のモデルを保存しました: {DIR_MERGED_MODEL}")
-
